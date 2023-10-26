7f2df213e82d9981214e3885b9b80e91fa78373c
Glider <2929799602@qq.com>
2023-10-26 10:51:39 +0800

    feat: 小球移动案例完成

--- html/canvas/case/index.js
+++ html/canvas/case/index.js
@@ -4,6 +4,7 @@ let canvas = document.getElementById("canvas");
 let ctx = canvas.getContext("2d");
 let { width, height } = canvas;
 
+// 移动的小球
 const radius = 10;
 const ball = new Ball(0, height / 2, radius, 10, 10);
 function drawBall() {
@@ -15,6 +16,14 @@ function drawBall() {
 
   // 判断边界x
   if (x + radius >= width) {
+    // 挡板没有碰到小球
+    if (
+      y < currentYPos - blockLength / 2 ||
+      y > currentYPos + blockLength / 2
+    ) {
+      // 不绘制小球
+      return;
+    }
     ball.xSpeed = -Math.abs(ball.xSpeed);
   } else if (x - radius <= 0) {
     ball.xSpeed = Math.abs(ball.xSpeed);
@@ -26,6 +35,7 @@ function drawBall() {
   } else if (y - radius <= 0) {
     ball.ySpeed = Math.abs(ball.ySpeed);
   }
+
   ctx.arc(ball.x, ball.y, radius, 0, Math.PI * 2);
   ctx.stroke();
   ctx.save();
@@ -33,11 +43,13 @@ function drawBall() {
   window.requestAnimationFrame(drawBall);
 }
 
-const blockLength = 50;
+// 右边界
+const blockLength = 100;
 const lineWidth = 10;
 function drawBlock() {
-  const yPos = currentYPos || height / 2;
+  const yPos = currentYPos;
   ctx.beginPath();
+  ctx.clearRect(width - lineWidth, 0, width, height);
   ctx.lineWidth = lineWidth;
   ctx.moveTo(width, yPos - blockLength / 2);
   ctx.lineTo(width, yPos + blockLength / 2);
@@ -52,7 +64,10 @@ function draw() {
   window.requestAnimationFrame(drawBlock);
 }
 
-let currentYPos = 0;
+/**
+ * 监听鼠标位置变化
+ */
+let currentYPos = height / 2;
 document.addEventListener("mousemove", (event) => {
   const { x, y } = event;
 
