ec2cacf04d0b9d48ab0adf7a8a4970b89563e9b4
Glider <2929799602@qq.com>
2023-11-09 20:04:25 +0800

    feat: [maze] 迷宫格子初始化

--- html/canvas/case/maze/index.js
+++ html/canvas/case/maze/index.js
@@ -3,8 +3,14 @@ function twoDimensionalArr(width, height) {
   for (let i = 0; i < width; i++) {
     let innerArr = [];
     for (let j = 0; j < height; j++) {
-      let direction = [];
-
+      // 设置代理x, y
+      let direction = new Proxy([], {
+        get(target, property) {
+          if (property === "x") return i;
+          if (property === "y") return j;
+          return target[property];
+        },
+      });
       for (let k = 0; k < 4; k++) {
         direction.push(true);
       }
@@ -19,20 +25,32 @@ function twoDimensionalArr(width, height) {
 const maze = twoDimensionalArr(3, 4);
 
 function getRandomSide(x, y, maxXIndex, maxYIndex) {
-  if (x + 1 <= maxXIndex && !maze[x + 1][y].isGet) return maze[x + 1][y];
+  if (x + 1 <= maxXIndex && !maze[x + 1][y].isGet) {
+    maze[x + 1][y][1] = false;
+    return maze[x + 1][y];
+  }
 
-  if (x - 1 >= 0 && !maze[x - 1][y].isGet) return maze[x - 1][y];
+  if (x - 1 >= 0 && !maze[x - 1][y].isGet) {
+    maze[x - 1][y][3] = false;
+    return maze[x - 1][y];
+  }
 
-  if (y + 1 <= maxYIndex && !maze[x][y + 1].isGet) return maze[x][y + 1];
+  if (y + 1 <= maxYIndex && !maze[x][y + 1].isGet) {
+    maze[x][y + 1][2] = false;
+    return maze[x][y + 1];
+  }
 
-  if (y - 1 >= 0 && !maze[x][y - 1].isGet) return maze[x][y - 1];
+  if (y - 1 >= 0 && !maze[x][y - 1].isGet) {
+    maze[x][y - 1][0] = false;
+    return maze[x][y - 1];
+  }
 
   return false;
 }
 
 function linkMaze(maze) {
   const entryPoint = maze[0][0];
-  const outputPoint = maze[maze.length - 1][maze[0].length - 1];
+  // const outputPoint = maze[maze.length - 1][maze[0].length - 1];
   const _getRandomSide = (x, y) =>
     getRandomSide(x, y, maze.length - 1, maze[0].length - 1);
   let totalNum = maze.length * maze[0].length;
@@ -43,9 +61,26 @@ function linkMaze(maze) {
   entryPoint.isGet = true;
   totalNum--;
 
-  while (totalNum) {
-    const currentGridItem = stack[stack.length - 1];
-    const nextGridItem = getRandomSide;
-    totalNum--;
+  while (stack.length || totalNum) {
+    const { x, y } = stack[stack.length - 1];
+
+    const nextGridItem = getRandomSide(
+      x,
+      y,
+      maze.length - 1,
+      maze[0].length - 1
+    );
+
+    if (nextGridItem) {
+      nextGridItem.isGet = true;
+      stack.push(nextGridItem);
+      totalNum--;
+    } else {
+      stack.pop();
+    }
   }
 }
+
+linkMaze(maze);
+
+console.log(maze);
